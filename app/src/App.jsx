5490688c7ae63507e38a1f532ee03e3568c2311a
misfasol <gabriel.schlagenhaufer@gmail.com>
2025-04-25 11:36:22 -0300

    remover import modal

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import './App.css'
 import DataList from './components/DataList'
 import Cadastro from './components/Cadastro';
-import Modal from './components/Modal';
 
 function App({users, setUsers, setUserSelecionado}) {
   // const [isModalOpen, setIsModalOpen] = useState(false);
